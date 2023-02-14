2c347c34bcd060152df43a98f8b50e2d14762c9a
rohit133 <rohit.12012980@lpu.in>
2023-02-14 16:34:10 +0530

    Level-1 Basic Header, footer and BodyContent

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import ContentBody from "./ContentBody";
+import Footer from "./Footer";
+//2. Create a App.jsx component.
+function App() {
+  return (
+    <div>
+      <Header />
+      <ContentBody />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+// Create a Header.jsx component that renders a <header> element
+// to show the Keeper App name in an <h1>.
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header h1">Keeper</h1>
+    </header>
+  );
+}
+export default Header;
